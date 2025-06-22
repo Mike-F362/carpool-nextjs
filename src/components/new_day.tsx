7d4360f1c9472ed015290fbfd543a68657f23212
Mike Feustel <mike.feustel@localhost>
2025-06-22 16:59:32 +0200

    new_day.tsx: removed unused param setAnwesenheiten

--- src/components/new_day.tsx
+++ src/components/new_day.tsx
@@ -29,7 +29,6 @@ export default function NeuerTag({
                                      setDaten,
                                      setNeuerTagAktiv,
                                      mitglieder,
-                                     setAnwesenheiten,
                                      startpunkt1,
                                      tableContainerRef,
                                      fahrtSpeichern,
