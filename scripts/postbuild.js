ec68d9dfac71a54adda92275a664a5230b41d8c7
Mike Feustel <mike.feustel@localhost>
2025-06-29 15:24:40 +0200

    refactoring postbuild.js

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,19 +1,23 @@
 const fs = require("fs");
 const path = require("path");
-const {execSync} = require("child_process");
+const { execSync } = require("child_process");
 
 const version = require("../package.json").version;
-const gitTag = execSync("git describe --tags --abbrev=0").toString().trim();
-const commitHash = execSync("git rev-parse HEAD").toString().trim();
+let gitTag = "";
+let commitHash = "";
 
-const envPath = path.join(__dirname, "../.env.local");
+try {
+    gitTag = execSync("git describe --tags --abbrev=0").toString().trim();
+    commitHash = execSync("git rev-parse HEAD").toString().trim();
+} catch (e) {
+    console.warn("Git-Daten konnten nicht ermittelt werden");
+}
 
-fs.writeFileSync(
-    envPath,
-    `NEXT_PUBLIC_APP_VERSION=v${version}
+const envContent = `
+NEXT_PUBLIC_APP_VERSION=v${version}
 NEXT_PUBLIC_GIT_TAG=${gitTag}
 NEXT_PUBLIC_COMMIT_HASH=${commitHash}
-`,
-);
+`;
 
-console.log("🔧 .env.local created with version and git info");
+fs.writeFileSync(path.resolve(__dirname, "../.env.local"), envContent.trim());
+console.log("✅ .env.local mit Versionsdaten erzeugt.");
