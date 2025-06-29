03d6e46d3c5d48dde770209fdf142bce9b6f1812
Mike Feustel <mike.feustel@localhost>
2025-06-29 11:06:56 +0200

    index,tsx, new_day.tsx: moved calc quotes to newday.tsx, use calc api

--- src/components/new_day.tsx
+++ src/components/new_day.tsx
@@ -4,6 +4,8 @@ import {supabase} from "@/lib/supabaseClient";
 import React, {RefObject, useState} from "react";
 import Tour from "@/interfaces/tour";
 import Driver from "@/interfaces/driver";
+import FahrerVorschlag from "@/interfaces/driver_suggestion";
+import {supabase} from "@/lib/supabaseClient";
 
 type Props = {
     anwesenheiten: Array<Set<string>>,
@@ -12,14 +14,14 @@ type Props = {
     setDaten: (d: any[]) => void,
     setNeuerTagAktiv: (v: boolean) => void,
     mitglieder: Driver[],
-    setAnwesenheiten: (liste: Set<string>[]) => void,
-    startpunkt1: string[],
+    setAnwesenheiten: (liste: Set<number>[]) => void,
+    startpunkt1: number[],
     tableContainerRef: React.RefObject<HTMLDivElement>,
-    zwischenstopp: string[],
-    fahrtSpeichern: (datum: Date, aktuelleAnwesenheit: Set<string>, aktuellerVorschlag: { fahrerA: string; fahrerB: string }) => void,
+    zwischenstopp: number[],
+    fahrtSpeichern: (datum: Date, aktuelleAnwesenheit: Set<number>, aktuellerVorschlag: FahrerVorschlag) => void,
     datum: Date,
-    simuliereFahrt: (anwesend: Set<string>, daten, anwesenheiten) => { fahrerA: string; fahrerB: string },
-    setDatum: (value: (((prevState: Date) => Date) | Date)) => void
+    setDatum: (value: (((prevState: Date) => Date) | Date)) => void,
+    ladeFahrten: () => Promise<void>
 };
 
 export default function NeuerTag({
@@ -34,16 +36,148 @@ export default function NeuerTag({
                                      fahrtSpeichern,
                                      zwischenstopp,
                                      datum,
-                                     simuliereFahrt,
-                                     setDatum
+                                     setDatum,
+                                     ladeFahrten
                                  }: Props) {
-    const [aktuellerVorschlag, setAktuellerVorschlag] = useState({fahrerA: "", fahrerB: ""});
-    const [aktuelleAnwesenheit, setAktuelleAnwesenheit] = useState(new Set<string>());
+    const [aktuellerVorschlag, setAktuellerVorschlag] = useState<FahrerVorschlag>({fahrerA_id: 0, fahrerB_id: 0});
+    const [aktuelleAnwesenheit, setAktuelleAnwesenheit] = useState(new Set<number>());
+    const [quotesZw, setQuotesZw] = useState(new Map<number, number>);
+    const [quotesSp, setQuotesSp] = useState(new Map<number, number>);
 
-    const toggleAnwesenheit = (name) => {
-        const kopie = new Set<string>(aktuelleAnwesenheit);
-        if (kopie.has(name)) kopie.delete(name);
-        else kopie.add(name);
+    const simulate = async () => {
+        const aktuelleAnwesenheit = new Set<number>(mitglieder.map(m => m.id));
+        const aktuellerVorschlag = await berechneFahrerVorschlag(aktuelleAnwesenheit, daten, anwesenheiten);
+
+        let datum = daten.map(d => d.datum).reduce((prev, curr, index, arr) => {
+            return prev > curr ? prev : curr
+        }, new Date());
+
+        let simDatum = new Date(datum);
+        do {
+            simDatum.setDate(simDatum.getDate() + 1);
+        } while (simDatum.getDay() === 0 || simDatum.getDay() === 6); // Sa+So überspringen
+
+        // const tag = daten.length + 1;
+        const anwesend = Array.from(aktuelleAnwesenheit);
+        const fahrer = aktuellerVorschlag;
+        const fahrt: Tour = {
+            datum: simDatum,
+            anwesend_ids: anwesend,
+            fahrerA_id: fahrer.fahrerA_id,
+            fahrerB_id: fahrer.fahrerB_id
+        };
+
+        await supabase.from("fahrten").insert(fahrt);
+
+        setAktuellerVorschlag(aktuellerVorschlag);
+        setDatum(simDatum);
+        setDaten([fahrt, ...daten]);
+        setAktuelleAnwesenheit(aktuelleAnwesenheit);
+
+        ladeFahrten();
+
+        setTimeout(() => {
+            tableContainerRef.current?.scrollTo({top: 0, behavior: 'smooth'});
+        }, 100);
+
+    }
+
+    async function ladeFahrerQuoteSp(anwesend: number[]): Promise<Map<number, number>> {
+        try {
+            const res = await fetch("/api/fahrer/quote", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({anwesend}),
+            });
+
+            if (!res.ok) {
+                console.error("Fehler beim Abrufen der Fahrerquote");
+                return;
+            }
+
+            const quotes: Object = await res.json();
+            const quoteMap: Map<number, number> = new Map(Object.entries(quotes).map(([key, value]) => {
+                return [parseInt(key), value];
+            }));
+            console.log("QuoteSp:", quoteMap);
+
+            setQuotesSp(quoteMap);
+
+            return quoteMap;
+        } catch (error) {
+            console.error("Netzwerkfehler:", error);
+        }
+    }
+
+    async function ladeFahrerQuoteZw(fahrerA_id: number, anwesend: number[]): Promise<Map<number, number>> {
+        try {
+            const res = await fetch("/api/fahrer/quote_zw", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({fahrerA_id: fahrerA_id, anwesend}),
+            });
+
+            if (!res.ok) {
+                console.error("Fehler beim Abrufen der Fahrerquote");
+                return;
+            }
+
+            const quotes: Object = await res.json();
+            const quoteMap: Map<number, number> = new Map(Object.entries(quotes).map(([key, value]) => {
+                return [parseInt(key), value];
+            }));
+            console.log("QuoteZw:", quoteMap);
+
+            setQuotesZw(quoteMap);
+
+            return quoteMap;
+        } catch (error) {
+            console.error("Netzwerkfehler:", error);
+        }
+    }
+
+    function nextDriver(anwesend2: number[], quoteZw: Map<number, number>) {
+        anwesend2.sort((a, b) => {
+            // TODO: bei Gleichstand: Datum letzte Fahrt vergleichen!
+            return ((quoteZw.get(a) | 0) - (quoteZw.get(b) | 0));
+        });
+
+        const fahrerB_id = anwesend2[0];
+        const fahrerB = fahrerListe.find(fahrer => fahrer.id == fahrerB_id)
+        const fahrerB_text = fahrerB || "?";
+        return {fahrer_id: fahrerB_id, fahrer_text: fahrerB_text};
+    }
+
+    async function berechneFahrerVorschlag(anwesend: Set<number>, daten, anwesenheiten): Promise<FahrerVorschlag> {
+        const anwesend1 = Array.from(anwesend)
+            .filter(n => startpunkt1.includes(n));
+
+        const quoteSp = await ladeFahrerQuoteSp(anwesend1);
+
+        const {fahrer_id: fahrerA_id, fahrer_text: fahrerA_text} = nextDriver(anwesend1, quoteSp);
+
+        console.log(`Fahrer A ${fahrerA_text} quote`, quoteSp);
+
+        const anwesend2 = Array.from(anwesend)
+            .filter(n => zwischenstopp.includes(n) && !startpunkt1.includes(n));
+
+        const quoteZw = await ladeFahrerQuoteZw(fahrerA_id, anwesend2);
+
+        anwesend2.push(fahrerA_id);
+        const {fahrer_id: fahrerB_id, fahrer_text: fahrerB_text} = nextDriver(anwesend2, quoteZw);
+
+        console.log(`Fahrer B ${fahrerB_text} quote`, quoteSp);
+
+        return {
+            fahrerA_id,
+            fahrerB_id
+        };
+    }
+
+    const toggleAnwesenheit = async (id: number) => {
+        const kopie = new Set<number>(aktuelleAnwesenheit);
+        if (kopie.has(id)) kopie.delete(id);
+        else kopie.add(id);
         setAktuelleAnwesenheit(kopie);
         setAktuellerVorschlag(simuliereFahrt(kopie, daten, anwesenheiten));
     };
---
+++ src/interfaces/driver_suggestion.ts
@@ -0,0 +1,4 @@
+export default interface FahrerVorschlag {
+    fahrerA_id: number,
+    fahrerB_id: number
+}
