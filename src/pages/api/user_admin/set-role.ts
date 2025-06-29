4cde5eb8dab06483bb2dcc787f6b7d67de6d1679
Mike Feustel <mike.feustel@localhost>
2025-06-29 11:03:00 +0200

    bugfix user_admin imports

--- src/pages/api/user_admin/set-role.ts
+++ src/pages/api/user_admin/set-role.ts
@@ -1,4 +1,4 @@
-import { createRouteHandlerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesServerClient as createRouteHandlerSupabaseClient} from '@supabase/auth-helpers-nextjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
