dd9078bf981ee9ccd7072d8cd50bf9f70a8399ac
Mike Feustel <mike.feustel@localhost>
2025-06-29 12:05:47 +0200

    added build script log output

--- vercel-build.sh
+++ vercel-build.sh
@@ -4,4 +4,7 @@ VERSION=$(git describe --tags --always)
 COMMIT=$(git rev-parse --short HEAD)
 
 mkdir -p public
+
+echo "Pushing version to json file: { \\"version\\": \\"$VERSION\\", \\"tag\\": \\"$VERSION\\", \\"commit\\": \\"$COMMIT\\" }"
+
 echo "{ \\"version\\": \\"$VERSION\\", \\"tag\\": \\"$VERSION\\", \\"commit\\": \\"$COMMIT\\" }" > public/version.json
